84d1d0bc971765b68254fe23ed1ddcff82921569
Shagun Mishra <mshagun2001@gmail.com>
2021-05-11 17:13:57 +0530

    added mongo

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from "express";
 // import bodyParser from "body-parser";
 import _ from "lodash";
 import cors from "cors";
+import mongoose from "mongoose";
+import moment from "moment";
 
 const data = "Working Properly!";
 const app = express();
@@ -13,13 +15,49 @@ app.use(express.json())
 app.use(express.static("public"));
 app.use(cors());
 
+mongoose.connect("mongodb://localhost/noveliaDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection error: "));
+
+// db.once("open", () => {
+
+// });
+
+const Schema = mongoose.Schema;
+
+const BookInfoSchema = new Schema({
+    BookID: Number,
+    Name: String,
+    Author: String,
+    DateOfCreation: Date,
+    NoOfChapter: Number
+});
+
+const BookInfo = mongoose.model("BookInfo", BookInfoSchema);
+
+let book1 = new BookInfo({
+    BookID: "12345",
+    Name: "The King of Drugs",
+    Author: "Nora Barrett",
+    DateOfCreation: moment("20140202", "YYYYMMDD"),
+    NoOfChapter: 50
+});
+
+book1.save(err => {
+    if (err) console.log(err);
+})
 
 app.get("/", (req, res) => {
     res.send(data);
 });
 
 app.get("/data", (req, res) => {
-    res.send(data);
+    BookInfo.findOne({Name: "The King of Drugs"}, (err, book) => {
+        if(err) console.log(err);
+        else res.send(book);
+    });
 });
 
 app.listen(9000, () => {
